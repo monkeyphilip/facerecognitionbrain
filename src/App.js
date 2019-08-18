ac911aacae2c152dfe4e809fb7afd47a0e70488e
monkeyphilip <monkey.philip32@gmail.com>
2019-08-18 00:45:36 -0400

    added all the building components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
 import './App.css';
+
+const particlesOptions ={
+  particles: {
+    number: {
+      value:30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles"
+              params={particlesOptions}
+            />
       <Navigation />
-      {/*<Logo />
-                <ImageLinkForm />
-                <FaceRecognition />*/}    
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+              {/*
+  <FaceRecognition />*/}    
       </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = ()=> {
 	return (
 		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-			<p>Sign Out</p>
+			<p className='f3 link dim black underline pa3 pointer light-gray'>Sign Out</p>
 		</nav>
 	);
 }
